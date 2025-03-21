aa0852eed47e6bf1f57ca7567db1870cec8d50d7
Kaloina RANDRIAMBOLOLONA <143845374+KaloinaRandria@users.noreply.github.com>
2025-03-21 13:38:23 +0300

    Creation fonction initBase()

--- src/main/java/site/easy/to/build/crm/service/DataManager/ResetData.java
+++ src/main/java/site/easy/to/build/crm/service/DataManager/ResetData.java
@@ -3,16 +3,31 @@ package site.easy.to.build.crm.service.DataManager;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Service;
+import javax.sql.DataSource;
 
 @Service
 public class ResetData {
     private final JdbcTemplate jdbcTemplate;
 
     @Autowired
-    public ResetData(JdbcTemplate jdbcTemplate) {
-        this.jdbcTemplate = jdbcTemplate;
+    public ResetData(DataSource dataSource) {
+        this.jdbcTemplate = new JdbcTemplate(dataSource);
     }
 
-    
+    public void initBase() {
+        jdbcTemplate.execute("SET FOREIGN_KEY_CHECKS = 0");
 
+        jdbcTemplate.execute("TRUNCATE TABLE contract_settings");
+        jdbcTemplate.execute("TRUNCATE TABLE email_templates");
+        jdbcTemplate.execute("TRUNCATE TABLE file");
+        jdbcTemplate.execute("TRUNCATE TABLE google_drive_file");
+        jdbcTemplate.execute("TRUNCATE TABLE lead_action");
+        jdbcTemplate.execute("TRUNCATE TABLE lead_settings");
+        jdbcTemplate.execute("TRUNCATE TABLE ticket_settings");
+        jdbcTemplate.execute("TRUNCATE TABLE trigger_contract");
+        jdbcTemplate.execute("TRUNCATE TABLE trigger_lead");
+        jdbcTemplate.execute("TRUNCATE TABLE trigger_ticket");
+
+        jdbcTemplate.execute("SET FOREIGN_KEY_CHECKS = 1");
+    }
 }
