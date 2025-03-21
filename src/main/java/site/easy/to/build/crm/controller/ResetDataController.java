a23bdda4c0d416a4254f8ba9fe0a48fdf677b6c4
Kaloina RANDRIAMBOLOLONA <143845374+KaloinaRandria@users.noreply.github.com>
2025-03-21 13:44:48 +0300

    Create ResetDataController.java

---
+++ src/main/java/site/easy/to/build/crm/controller/ResetDataController.java
@@ -0,0 +1,19 @@
+package site.easy.to.build.crm.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import site.easy.to.build.crm.service.DataManager.ResetData;
+
+@Controller
+public class ResetDataController {
+    @Autowired
+    ResetData resetData;
+
+    @GetMapping("/delete-data")
+    public String supprimerDonnee() {
+        resetData.initBase();
+        return "redirect:/";
+    }
+
+}
