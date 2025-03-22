aa87c995b90c1eee38bfd04f5dbdd79edd827fa5
Kaloina RANDRIAMBOLOLONA <143845374+KaloinaRandria@users.noreply.github.com>
2025-03-22 13:01:11 +0300

    Depense Service

--- src/main/java/site/easy/to/build/crm/service/my/BudgetService.java
+++ src/main/java/site/easy/to/build/crm/service/my/BudgetService.java
@@ -2,12 +2,25 @@ package site.easy.to.build.crm.service.my;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import site.easy.to.build.crm.entity.my.Budget;
 import site.easy.to.build.crm.repository.my.BudgetRepository;
+
+import java.util.List;
 
 @Service
 public class BudgetService {
     @Autowired
     BudgetRepository budgetRepository;
 
-    
+    public void saveBudget(Budget budget) {
+        budgetRepository.save(budget);
+    }
+
+    public List<Budget> getAllBudgets() {
+        return budgetRepository.findAll();
+    }
+
+    public Budget getBudgetById(Integer id) {
+        return budgetRepository.findById(id).orElse(null);
+    }
 }
---
+++ src/main/java/site/easy/to/build/crm/service/my/DepenseService.java
@@ -0,0 +1,26 @@
+package site.easy.to.build.crm.service.my;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import site.easy.to.build.crm.entity.my.Depense;
+import site.easy.to.build.crm.repository.my.DepenseRepository;
+
+import java.util.List;
+
+@Service
+public class DepenseService {
+    @Autowired
+    DepenseRepository depenseRepository;
+
+    public void saveDepense(Depense depense) {
+        depenseRepository.save(depense);
+    }
+    public Depense getDepense(Integer id) {
+        return depenseRepository.findById(id).orElse(null);
+    }
+
+    public List<Depense> getAllDepenses() {
+        return depenseRepository.findAll();
+    }
+    
+}
